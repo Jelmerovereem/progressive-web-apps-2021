ce27eb730d1aa4300f0ecd384612234747d11267
Jelmer Overeem <jelmer_overeem@hotmail.com>
2021-03-08 15:45:40 +0100

    added rendering for pug

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 // require express
 const express = require("express");
 
+const pug = require("pug");
+
 const app = express();
 
 app.use(express.static("static"));
-
+//app.set("view engine", "ejs");
+app.set("view engine", "pug");
 app.get("/", renderHome);
 
 function renderHome(req, res) {
-	res.send("Hello world");
+	const title = "Weather app";
+	res.render("home", {
+		pageTitle: title // pass data as an object
+	});
 }
 
 app.listen(7000, () => console.log("Server is running on port 7000"));
