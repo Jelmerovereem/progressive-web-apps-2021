deb014570a754cb68fdf80c9210006ffc16b64fb
Jelmer Overeem <jelmer_overeem@hotmail.com>
2021-03-09 16:21:31 +0100

    added render to map module

---
+++ static/scripts/modules/toMap.js
@@ -0,0 +1,20 @@
+import { leafletMap } from "../utils/mapUtils.js";
+
+const iconsUrl = `http://openweathermap.org/img/wn/`;
+
+export default function toMap(weatherData) {
+	const coordinates = weatherData.coord;
+	const temp = Math.round(weatherData.main.temp);
+	const city = weatherData.name;
+	const icon = weatherData.weather[0].icon;
+	leafletMap.setView(coordinates, 13);
+	const marker = L.marker(coordinates).addTo(leafletMap);
+	const markerHtml = `
+		<div class="temp-header">
+			<span>${temp}°C</span>
+			<img src="${iconsUrl}${icon}@2x.png" class="weatherIcon" alt="weather-icon">
+		</div>
+		<p><a href="/${city}">${city}</a></p>
+	`;
+	marker.bindPopup(markerHtml).openPopup();
+}
