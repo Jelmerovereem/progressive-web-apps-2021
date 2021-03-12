402bbc093841a0209bec1af1c99f7857a9f813e9
Jelmer Overeem <jelmer_overeem@hotmail.com>
2021-03-12 14:28:42 +0100

    added heroku port

--- server.js
+++ server.js
@@ -81,4 +81,4 @@ async function getWeatherData(req, res) {
 	res.send({weatherData});
 }
 
-app.listen(7000, () => console.log("Server is running on port 7000"));
+app.listen(process.env.PORT || 7000, () => console.log("Server is running on port 7000"));
