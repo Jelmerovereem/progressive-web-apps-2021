e885b1d5ee4d2e92e34f6272ac6fad86d76c8475
Jelmer Overeem <jelmer_overeem@hotmail.com>
2021-03-12 10:38:09 +0100

    added modules

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ const urlencodedParser = bodyParser.urlencoded({
 import fetch from "node-fetch";
 //const fetch = require("node-fetch");
 
+import fetchData from "./modules/fetchData.js";
+
+import backgroundCss from "./modules/backgroundCss.js";
+
 const app = express();
 
 //app.use(express.static("static"));
@@ -41,15 +45,19 @@ function renderHome(req, res) {
 
 async function renderDetail(req, res) {
 	const city = req.params.city;
-	const weatherData = await fetch(`${weatherApiConfig.baseUrl}?q=${city}&units=${weatherApiConfig.units}&appid=${weatherApiConfig.apiKey}`)
-		.then(res => res.ok ? res.json() : res.ok);
+
+	const url = `${weatherApiConfig.baseUrl}?q=${city}&units=${weatherApiConfig.units}&appid=${weatherApiConfig.apiKey}`;
+	const weatherData = await fetchData(url);
 
 	console.log(weatherData);
 	console.log(new Date(weatherData.dt * 1000))
+	const weatherDesc = weatherData.weather[0].main.toLowerCase();
+	const backgroundGradient = await backgroundCss(weatherDesc);
 	
 	res.render("detail", {
 		weatherData,
-		pageTitle: `Weather app - ${city}`
+		pageTitle: `Weather app - ${city}`,
+		backgroundGradient: backgroundGradient
 	});
 }
 
@@ -61,8 +69,13 @@ async function getWeatherData(req, res) {
 	} else if (req.body.type === "coords") {
 		searchString = `lat=${req.body.coords.lat}&lon=${req.body.coords.lon}`;
 	}
-	const weatherData = await fetch(`${weatherApiConfig.baseUrl}?${searchString}&units=${weatherApiConfig.units}&appid=${weatherApiConfig.apiKey}`)
-	.then(res => res.ok ? res.json() : res.ok);
+	
+	const url = `${weatherApiConfig.baseUrl}?${searchString}&units=${weatherApiConfig.units}&appid=${weatherApiConfig.apiKey}`;
+	const weatherData = await fetchData(url);
+	console.log(weatherData)
+
+/*	const weatherData = await fetch(`${weatherApiConfig.baseUrl}?${searchString}&units=${weatherApiConfig.units}&appid=${weatherApiConfig.apiKey}`)
+	.then(res => res.ok ? res.json() : res.ok);*/
 	
 	res.setHeader("Content-Type", "application/json");
 	res.send({weatherData});
