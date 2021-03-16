c4b05143390e1d654497ccd1c73ca49eaca7aa4d
Jelmer Overeem <jelmer_overeem@hotmail.com>
2021-03-16 16:27:14 +0100

    added service worker to building

--- scripts/build-assets.js
+++ scripts/build-assets.js
@@ -1,4 +1,7 @@
 import gulp from "gulp";
 
-gulp.src("./static/manifest.json").pipe(gulp.dest("./dist/"))
+gulp.src([
+	"./static/manifest.json",
+	"./static/service-worker.js"
+	]).pipe(gulp.dest("./dist/"))
 gulp.src("./static/assets/**/*.*").pipe(gulp.dest("./dist/assets/"))
